64d96dd0b996fa923be077e5b0c62cbe86fa4677
Corbin <corbinisnotyou@gmail.com>
2022-05-13 10:47:10 -0400

    advice widget

--- dist/js/portal.js
+++ dist/js/portal.js
@@ -194,7 +194,7 @@ var helpers = {
 
 let layoutTemplates = [
     {name: "dashboard", page: "dashboard", desc: "Default widgets saving space", code:  ["Quote", "Good", "Happy", "Fun", "Dadjokes", "BibleVOD", "AM","Epic", "BoredPanda", "Colossal", "Godtube","ChloeCorner", "Zoo", "NineGag",  "CSSTricks", "Photo", "PhotoNatgeo",   "PhotoNasa", "News", "Todo", "Instagram", "Facts", "DailyFacts", "GoodNewsHuff", "Buzzfeed", "HealthGazette","ExcellentTown", "BeautifulMess", "DailyGood", "Icr", "Lifehacker" ]},
-    {name:"good", page: "good", desc: "", code: ["Quote", "Good", "Happy", "BibleVOD", "CleanMemes", "GoodNewsNetwork",  "SunnySkies", "Positive", "Godtube", "BibleTrivia", "DailyGood",  "TotesAcorbs|col-sm-6","CorbinBlog|col-sm-6", "ChristianHeadlines" ]},
+    {name:"good", page: "good", desc: "", code: ["Quote", "Good", "Happy", "BibleVOD", "CleanMemes", "GoodNewsNetwork",  "SunnySkies", "Positive", "Godtube", "BibleTrivia", "DailyGood",  "TotesAcorbs|col-sm-6","CorbinBlog|col-sm-6", "ChristianHeadlines", "Advice" ]},
     {name:"learn", page: "learn", desc: "", code: ["MakeUseOf|col-sm-12","Colossal", "Lifehacker|col-sm-6", "DailyFacts",  "WordOfDay", "Icr",    "DevTo", "PhotoNasa",  "HealthGazette|col-sm-4", "ExcellentTown|col-sm-4", "CSSTricks", "AM", "Britannica", "PinterestGreenscape"]},
     {name:"funny", page: "funny", desc: "", code: ["Dadjokes", "NineGag", "Todo|col-sm-4", "BoredPanda|col-sm-12", "BabyGoats|col-sm-6", "TinyHomes|col-sm-6", "Buzzfeed", "Epic", "Madlib|col-sm-4", "PhotoNatgeo", "AutoEvolution"]},
 ];
